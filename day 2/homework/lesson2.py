0b2a83e93cd1d411bd9d908965d66d097d2009e9
Saba936 <sabatsertsvadze42@gmail.com>
2025-05-09 18:41:06 +0400

    commit

---
+++ day 2/homework/lesson2.py
@@ -0,0 +1,26 @@
+name = input()
+print( name )
+
+#მიმატება
+50+50
+"saba" + "tsertsvadze"
+
+#გამოკლება
+10-5
+100-99
+
+#გამრავლება
+10*5
+"Saba"*2
+
+#გაყოფა
+10/2
+100/50
+
+name = "Saba"
+surname = "Tsertsvadze"
+age = "16"
+
+print(name)
+print(surname)
+print(age)
---
+++ day 3/homework/lesson3.py
@@ -0,0 +1,24 @@
+name = "Saba"
+surname = "Tsertsvadze"
+age = "16"
+
+print(name + surname + age)
+
+print( "Hello, my name is " + name + " and my surname is " + surname + ", i am " + age + " years old" )
+
+number_1 = 10
+number_2 = 7
+number_3 = 2
+number_4 = 1000
+number_5 = 50
+
+print(number_1 + number_2)
+print(number_4 - number_5)
+print(number_1 * number_4)
+print(number_5 / number_1)
+
+
+
+
+
+
